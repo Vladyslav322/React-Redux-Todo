648be605db59b134a585c7e50d9e1799d6dfea47
Vladyslav Varvianskyi <pol92leon@gmail.com>
2022-12-10 17:05:59 +0200

    Add redux actions, reducer

---
+++ src/redux/actionsTodo.js
@@ -0,0 +1,29 @@
+export const addTodo = (todo) => ({
+    type: 'ADD_TODO',
+    todo,
+});
+
+export const deleteTodo = (todoId) => ({
+    type: 'DELETE_TODO',
+    todoId,
+});
+
+export const completedTodo = (todoId) => ({
+    type: 'COMPLETED_TODO',
+    todoId,
+});
+
+export const editTodo = (todoId, changes) => ({
+    type: 'EDIT_TODO',
+    todo: { todoId, changes },
+});
+
+export const inProgressTodo = (todoId) => ({
+    type: 'IN_PROGRESS_TODO',
+    todoId,
+});
+
+export const openTodo = (todoId) => ({
+    type: 'OPEN_TODO',
+    todoId,
+});
---
+++ src/redux/todo.reducer.js
@@ -0,0 +1,65 @@
+const initState = {
+    todos: [],
+}
+
+function reducer(state = initState, action) {
+    switch (action.type) {
+        case 'ADD_TODO': {
+            const todos = [action.todo, ...state.todos];
+
+            return { ...state, todos };
+        }
+        case 'DELETE_TODO': {
+            const todos = state.todos.filter((element) => element.id !== action.todoId);
+
+            return { ...state, todos };
+        }
+        case 'COMPLETED_TODO': {
+            const todos = state.todos.map((item) => {
+                if (item.id === action.todoId) {
+                    return {
+                        ...item,
+                        status: 'completed'
+                    };
+                }
+
+                return { ...state, todos };
+            })
+        }
+        case 'IN_PROGRESS': {
+            const todos = state.todos.map((item) => {
+                if (item.id === action.todoId) {
+                    return {
+                        ...item,
+                        status: 'in-progress'
+                    }
+                }
+
+                return { ...state, todos }
+            })
+        }
+        case 'OPEN_TODO': {
+            const todos = state.todos.map((item) => {
+                if (item.id === action.todoId) {
+                    return {
+                        ...item,
+                        status: 'open-todo'
+                    }
+                }
+
+                return { ...state, todos }
+            })
+        }
+        case 'EDIT_TODO': {
+            const todos = state.todos.map((item) => {
+                if (item.id === action.todo.todoId) {
+                    return {
+                        ...item,
+                        ...action.todo.changes,
+                        updateDate: Date.now(),
+                    }
+                }
+            })
+        }
+    }
+}
